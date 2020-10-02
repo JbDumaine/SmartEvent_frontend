c5439307172a3cb707a3d202fbf57fd9cfe6971f
jbdumaine <dumaine.jb31@gmail.com>
2020-10-02 16:54:11 +0100

    add login in read me + fixe double quotes

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = 'https://reqres.in/api/'
+const API_URL = `https://reqres.in/api/`
 
 class UserService {
   getPublicContent() {
-    return axios.get(API_URL + 'all')
+    return axios.get(API_URL + `all`)
   }
 
   getUserHome() {
-    return axios.get(API_URL + 'user', { headers: authHeader() })
+    return axios.get(API_URL + `user`, { headers: authHeader() })
   }
 
 
